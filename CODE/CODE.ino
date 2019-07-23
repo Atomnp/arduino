22543eed8980d21a1b91cf774d7bd59b4831d0a2
kanu <aayushshah196@gmail.com>
2019-07-23 07:42:09 +0545

    lm293d added and code changed accordingly

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -1,44 +1,45 @@
-#define LED1 3
-#define LED2 2
-#define LED3 5
-#define LED4 4
-#define SENSOR3 7
-#define SENSOR6 9
+
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define LED4 5
+#define SENSOR1 8
+#define SENSOR2 9
+#define Enable 5
  
 void setup() {
-  Serial.begin(9600);
   pinMode(LED1 ,OUTPUT);
   pinMode(LED2 ,OUTPUT);
   pinMode(LED3 ,OUTPUT);
   pinMode(LED4 ,OUTPUT);
-  pinMode(SENSOR3,INPUT);
-  pinMode(SENSOR6,INPUT);
+  pinMode(SENSOR1,INPUT);
+  pinMode(SENSOR2,INPUT);
+  pinMode (Enable , OUTPUT);
   
-  // put your setup code here, to run once:
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-if( (digitalRead(SENSOR3) == HIGH)&& (digitalRead(SENSOR6)==HIGH) ){
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,HIGH);
-  digitalWrite(LED4,LOW);
-  
-  }
-  else if( (digitalRead(SENSOR3) == HIGH)&& (digitalRead(SENSOR6) == LOW ) ){
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,HIGH);
-  digitalWrite(LED3,HIGH);
-  digitalWrite(LED4,LOW);
-    }
 
-    else if( (digitalRead(SENSOR3) == LOW )&& (digitalRead(SENSOR6) == HIGH ) ){
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED4,HIGH);
-    }
+  digitalWrite(Enable,HIGH);
+  if( ( digitalRead(SENSOR1) == HIGH )&& ( digitalRead(SENSOR2) == HIGH ) ){
+    digitalWrite(LED1,HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    digitalWrite(LED4,LOW);
+  }
+  
+  else if( (digitalRead(SENSOR1) == HIGH ) && (digitalRead(SENSOR2) == LOW ) ){
+    digitalWrite(LED1,HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,HIGH);
+  }
+
+  else if( ( digitalRead(SENSOR1) == LOW )&& ( digitalRead(SENSOR2) == HIGH ) ){
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,HIGH);
+    digitalWrite(LED4,LOW);
+  }
 
 }
