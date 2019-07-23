22d0af1cdcf084540d19d5b813b4f1751e502a85
kanu <aayushshah196@gmail.com>
2019-07-23 09:39:48 +0545

    final modification

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -1,46 +1,54 @@
-
-#define MOTOR1 2
-#define MOTOR2 3
-#define MOTOR3 5
-#define MOTOR4 6
-#define SENSOR1 8
-#define SENSOR2 9
-#define Enable 4
- 
-void setup() {
-  pinMode(MOTOR1 ,OUTPUT);
-  pinMode(MOTOR2 ,OUTPUT);
-  pinMode(MOTOR3 ,OUTPUT);
-  pinMode(MOTOR4 ,OUTPUT);
-  pinMode(SENSOR1,INPUT);
-  pinMode(SENSOR2,INPUT);
-  pinMode (Enable , OUTPUT);
-  digitalWrite(Enable,HIGH);
   
-}
-
-void loop() {
-
-  
-  if( ( digitalRead(SENSOR1) == HIGH )&& ( digitalRead(SENSOR2) == HIGH ) ){
-    digitalWrite(MOTOR1,HIGH);
-    digitalWrite(MOTOR2,LOW);
-    digitalWrite(MOTOR3,HIGH);
-    digitalWrite(MOTOR4,LOW);
+  #define MOTOR1 2    //Connected to pin 2 of L293D
+  #define MOTOR2 3    //Connected to pin 7 of L293D 
+  #define MOTOR3 5    //Connected to pin 10 of L293D
+  #define MOTOR4 6    //Connected to pin 15 of L293D
+  #define IR1 8       //Left Sensor
+  #define IR2 9       //Right Sensor
+  #define Enable 4    //Connected to pin En1 & En2 of L293D
+   
+  void setup() {
+    
+    pinMode(MOTOR1 ,OUTPUT);
+    pinMode(MOTOR2 ,OUTPUT);
+    pinMode(MOTOR3 ,OUTPUT);
+    pinMode(MOTOR4 ,OUTPUT);
+    pinMode(IR1,INPUT);
+    pinMode(IR2,INPUT);
+    pinMode (Enable , OUTPUT);
+    
   }
   
-  else if( (digitalRead(SENSOR1) == HIGH ) && (digitalRead(SENSOR2) == LOW ) ){
-    digitalWrite(MOTOR1,HIGH);
-    digitalWrite(MOTOR2,LOW);
-    digitalWrite(MOTOR3,LOW);
-    digitalWrite(MOTOR4,HIGH);
+  void loop() {
+   
+    if( digitalRead(IR1)&& digitalRead(IR2) ){
+      digitalWrite(Enable,HIGH);
+      digitalWrite(MOTOR1,HIGH);
+      digitalWrite(MOTOR2,LOW);
+      digitalWrite(MOTOR3,HIGH);
+      digitalWrite(MOTOR4,LOW);
+    }
+    
+    else if( digitalRead(IR1) && !digitalRead(IR2) ){
+      digitalWrite(Enable,HIGH);
+      digitalWrite(MOTOR1,HIGH);
+      digitalWrite(MOTOR2,LOW);
+      digitalWrite(MOTOR3,LOW);
+      digitalWrite(MOTOR4,HIGH);
+    }
+  
+    else if( !digitalRead(IR1) && digitalRead(IR2) ){
+      digitalWrite(Enable,HIGH);
+      digitalWrite(MOTOR1,LOW);
+      digitalWrite(MOTOR2,HIGH);
+      digitalWrite(MOTOR3,HIGH);
+      digitalWrite(MOTOR4,LOW);
+    }
+  
+    else if( !digitalRead(IR1) && !digitalRead(IR2) ){
+      digitalWrite(Enable,LOW);
+    }
+  
+    delay(500);
+  
   }
-
-  else if( ( digitalRead(SENSOR1) == LOW )&& ( digitalRead(SENSOR2) == HIGH ) ){
-    digitalWrite(MOTOR1,LOW);
-    digitalWrite(MOTOR2,HIGH);
-    digitalWrite(MOTOR3,HIGH);
-    digitalWrite(MOTOR4,LOW);
-  }
-
-}
